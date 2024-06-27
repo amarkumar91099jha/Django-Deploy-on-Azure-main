61ca43966045644f1b5d4f83723e94b89daf4dcd
amarkumar91099jha <amar@seedsindia.org>
2024-06-27 12:00:58 +0530

    added header footer and merut and lucknow flood in map

--- azure_content/views.py
+++ azure_content/views.py
@@ -16,6 +16,26 @@ import geojson
 from django.contrib.auth.decorators import login_required
 
 
+
+def lucknow_flood_geojson_view(request):
+    geojson_file = os.path.join(os.path.dirname(__file__), 'static/azure_content/lucknow_floood_final.geojson')
+    if os.path.exists(geojson_file):
+        with open(geojson_file, 'r') as f:
+            geojson_data = json.load(f)
+        return JsonResponse(geojson_data, safe=False)
+    else:
+        return HttpResponse("File not found.", status=404)
+
+def merut_flood_geojson_view(request):
+    geojson_file = os.path.join(os.path.dirname(__file__), 'static/azure_content/meerut_flood_final.geojson')
+    if os.path.exists(geojson_file):
+        with open(geojson_file, 'r') as f:
+            geojson_data = json.load(f)
+        return JsonResponse(geojson_data, safe=False)
+    else:
+        return HttpResponse("File not found.", status=404)
+
+
 def geojson_view(request):
     geojson_file = os.path.join(os.path.dirname(__file__), 'static/azure_content/chennai_final_house_level_4326.geojson')
     if os.path.exists(geojson_file):
